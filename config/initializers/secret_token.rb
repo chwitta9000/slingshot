ed918d4de0bc906ba022833bdba1b9a026756c34
Charles Whittaker <charles.whittaker@gmail.com>
2014-03-08 22:29:58 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slingshot::Application.config.secret_token = '0c6a6cb208ab44a9a3e67532dec3674407961db326f894e30b1a2af28dfd40fb60d8cec04c122c2f3f01b88beb0744b044606e20cb03265dd3a759edb05c1671'
