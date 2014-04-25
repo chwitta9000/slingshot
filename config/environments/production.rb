c9092825c7c7f120b8bef14ee357b069eadc658f
Charles Whittaker <charles.whittaker@gmail.com>
2014-04-25 12:24:54 -0500

    add production stuffz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ Slingshot::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # http://slingshot-prod.herokuapp.com
-  config.action_mailer.default_url_options = { :host => 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'slingshot-prod.herokuapp.com' }
 end
